e7dd965d9ba3eedcd30a5a4256d90177715bb556
99063871 <99063871@mydavinci.com>
2021-12-15 09:53:26 +0100

    bonnetje gemaakt

--- ijssallon.py
+++ ijssallon.py
@@ -1,3 +1,11 @@
+bolletjes = 0
+horrentje = 0
+bakje = 0
+
+bolletjesPrijs = 1.10
+horrentjePrijs = 1.25
+bakjePrijs = 0.75
+
 print("Welkom bij Papi Gelato.")
 
 def welkeSmaakFunc():
@@ -10,12 +18,16 @@ def welkeSmaakFunc():
 
 def papiGelatoFunc():
     global amount
+    global bolletjes
+    global bakje
     amount = input("Hoeveel bolletjes wilt u?")
+    bolletjes += int(amount)
     welkeSmaakFunc()
     if amount <= "3":
         inWhat()
     elif amount >= "4" and amount <= "8":
         bakOfhoorn = 'bakje'
+        bakje +=1
         orderMoreFunc()
     elif amount > "8" and amount == int:
         print("Sorry, zulke grote bakken hebben we niet")
@@ -30,12 +42,15 @@ def orderMoreFunc():
         papiGelatoFunc()
     elif another == "n":
         print("Bedankt en tot ziens!")
+        bonnetje()
     elif another !="y" or another != "n":
         print("Sorry, dat snap ik niet...")
         orderMoreFunc()
 
 def inWhat():
     global bakOfhoorn
+    global horrentje
+    global bakje
     bakOfhoorn = input("Wilt u deze " + str(amount) + " bolletje(s) in \nA) een hoorntje of \nB) een bakje?")
     if bakOfhoorn != "a" and bakOfhoorn != "b":
         print("Sorry dat snap ik niet...")
@@ -43,8 +58,25 @@ def inWhat():
     else:
         if bakOfhoorn == "a":
             bakOfhoorn = "hoorntje"
+            horrentje += 1
         elif bakOfhoorn == "b":
             bakOfhoorn = 'bakje'
+            bakje += 1
         orderMoreFunc()
         
+
+def bonnetje():
+    if bolletjes != 0:
+        bolletjesTotal = bolletjes*bolletjesPrijs
+        horrentjeTotal = horrentje*horrentjePrijs
+        bakjeTotal = bakje*bakjePrijs
+        total = bolletjesTotal + horrentje + bakjeTotal
+        print('---------["Papi Gelato"]---------\n')
+        print('Bolletjes     ',bolletjes, ' x €1.10   =€',bolletjesTotal)
+        if horrentje != 0:
+            print('horrentje     ',horrentje, ' x €1.25   =€',horrentjeTotal)
+        elif bakje != 0:
+            print('bakje         ',bakje, ' x €1.10   =€',bakjeTotal)
+        print('                           -------- +')
+        print('Totaal                      =€',total)
 papiGelatoFunc()
