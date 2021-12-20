6b2b331b437fbe04ae094b8ccf2376c9492e304f
99063871 <99063871@mydavinci.com>
2021-12-20 12:05:15 +0100

    Foutmelding veranderd, en code verbeterd

--- ijssallon.py
+++ ijssallon.py
@@ -13,6 +13,8 @@ toppingTotalPrice = 0
 slagroomprice = 0.50
 sprinkelsprice = 0.30
 
+errorMessages = "Sorry dat is geen optie die we aanbieden..."
+
 print("Welkom bij Papi Gelato.")
 def whoFunc():
     who = input("Bent u 1) particulier of 2) zakelijk?")
@@ -23,7 +25,7 @@ def whoFunc():
             for a in range(1, int(amount) + 1):
                 smaak = input("Welke smaak wilt u voor bolletje nummer "+ str(a) +"? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?").lower()
                 if smaak != 'a' and smaak != 'c' and smaak != 'm' and smaak != 'v':
-                    print("Sorry dat snap ik niet...")
+                    print(errorMessages)
                     welkeSmaakFunc()
 
         def papiGelatoFunc():
@@ -45,7 +47,7 @@ def whoFunc():
                 print("Sorry, zulke grote bakken hebben we niet")
                 papiGelatoFunc()
             else:
-                print("Sorry dat snap ik niet...")
+                print(errorMessages)
                 papiGelatoFunc()
 
         def orderMoreFunc():
@@ -57,7 +59,7 @@ def whoFunc():
                 print("Bedankt en tot ziens!")
                 bonnetje()
             elif another !="y" or another != "n":
-                print("Sorry, dat snap ik niet...")
+                print(errorMessages)
                 orderMoreFunc()
 
         def inWhat():
@@ -65,7 +67,7 @@ def whoFunc():
             global bakje
             bakOfhoorn = input("Wilt u deze " + str(amount) + " bolletje(s) in \nA) een hoorntje of \nB) een bakje?")
             if bakOfhoorn != "a" and bakOfhoorn != "b":
-                print("Sorry dat snap ik niet...")
+                print(errorMessages)
                 inWhat()
             else:
                 if bakOfhoorn == "a":
@@ -96,7 +98,7 @@ def whoFunc():
                     elif bakOfhoorn == "bakje":
                         toppingTotalPrice += 0.90
                 else:
-                    print("Sorry dat snap ik niet...")
+                    print(errorMessages)
                     toppingFunc()
 
         def bonnetje():
@@ -122,7 +124,7 @@ def whoFunc():
     elif who == "2":
         zakelijk.zakelijk()
     else:
-        print("Sorry dat snap ik niet...")
+        print(errorMessages)
         whoFunc()
 
 whoFunc()
--- zakelijk.py
+++ zakelijk.py
@@ -1,4 +1,4 @@
-
+errorMessages = "Sorry dat is geen optie die we aanbieden..."
 
 def zakelijk():
     global liter
@@ -8,16 +8,16 @@ def zakelijk():
     
 def welkeSmaakFunc():
     for a in range(1, int(liter) + 1):
-        smaak = input("Welke smaak wilt u voor liter nummer "+ str(a) +"? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?").lower()
+        smaak = input("Welke smaak wilt u voor liter nummer "+ str(a) +"? A) Aardbei, C) Chocolade of V) Vanille?").lower()
         if smaak != 'a' and smaak != 'c' and smaak != 'm' and smaak != 'v':
-            print("Sorry dat snap ik niet...")
+            print(errorMessages)
             welkeSmaakFunc()
 
 def bonnetje():
     price = int(liter)*9.80
-    btw = price/109*9
+    btw = price/109*6
     print('---------["Papi Gelato"]---------\n')
     print('Liter     ',liter, ' x €9.80   =€',round(price, 2))
     print('                           -------- +')
     print('Totaal                  = €',round(price ,2))
-    print('BTW (9%)                = €',round(btw, 2))
+    print('BTW (6%)                = €',round(btw, 2))
